568e12ba8db36dc65994df0f5a2b5ffc366e6b46
karnponpoochitkanon <karnpon.p@ku.th>
2025-08-11 14:24:28 +0700

    CRUD completed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = 1500;
+const PORT = 15000;
 
 const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/todos';
 
