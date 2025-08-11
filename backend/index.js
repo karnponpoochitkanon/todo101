52354915b426d4fdc7c87dd125c7756f1f795ed9
karnponpoochitkanon <karnpon.p@ku.th>
2025-08-11 15:51:54 +0700

    fix backend code

--- backend/index.js
+++ backend/index.js
@@ -33,8 +33,8 @@ app.put('/tasks/:id', async (req, res) => {
 });
 
 app.delete('/tasks/:id', async (req, res) => {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    res.json(task);
+    await Task.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
 }
 );
 
@@ -53,8 +53,8 @@ const connectWithRetry = () => {
 
       })
       .catch(err => {
-        console,error('MongoDB connection error. Retrying in 5s...', err.massage);
-        srtTimeout(connectWithRetry, 5000);
+        console.error('MongoDB connection error. Retrying in 5s...', err.message);
+        setTimeout(connectWithRetry, 5000);
       });
 };
 
